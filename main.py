6d8dac78f0e305101cb86f755cfb3176fb044467
Joao Zsigmond <jzsiggy@gmail.com>
2023-06-07 20:36:01 -0300

    finito

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ onlyfiles = [f for f in listdir("./inputs") if isfile(join("./inputs", f))]
 for f in onlyfiles:
     system("./greedy/main.out < inputs/{input} > outputs/greedy/{output}".format(input=f, output=f.replace("input", "output")))
     system("./random/main.out < inputs/{input} > outputs/random/{output}".format(input=f, output=f.replace("input", "output")))
+
+# for f in onlyfiles:
+#     system("./parallel.out < inputs/{input} > outputs/parallel/{output}".format(input=f, output=f.replace("input", "output")))
+#     system("./iterative.out < inputs/{input} > outputs/iterative/{output}".format(input=f, output=f.replace("input", "output")))
+#     system("./gpu.out < inputs/{input} > outputs/gpu/{output}".format(input=f, output=f.replace("input", "output")))
